834363e3b3d853ec8e8e0907bac4a6864a6c7060
Benoit <benoit.maillard@epfl.ch>
2020-02-27 19:11:40 +0100

    Add useless hello world

--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("Hello");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
